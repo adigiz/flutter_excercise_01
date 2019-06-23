4204db86fe765c91680de3a730dc6e74cb1e215f
Gizwanda <adigiz@yahoo.com>
2019-06-24 00:54:09 +0700

    [Giz] add function to move to a different page for favourite words

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,36 @@ class RandomWordsState extends State<RandomWords> {
   final Set<WordPair> _saved = Set<WordPair>();
   final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
 
+  void _pushSaved() {
+    Navigator.of(context).push(
+        MaterialPageRoute<void>(
+            builder: (BuildContext context) {
+              final Iterable<ListTile> tiles = _saved.map(
+                      (WordPair pair) {
+                    return ListTile(
+                      title: Text(
+                        pair.asPascalCase,
+                        style: _biggerFont,
+                      ),
+                    );
+                  }
+              );
+              final List<Widget> divided = ListTile
+                  .divideTiles(
+                context: context,
+                tiles: tiles,
+              ).toList();
+              return Scaffold(
+                appBar: AppBar(
+                  title: Text('Saved Suggestions'),
+                ),
+                body: ListView(children: divided,),
+              );
+            }
+        )
+    );
+  }
+
   Widget _buildSuggestions() {
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
@@ -59,6 +89,9 @@ class RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)
+        ],
       ),
       body: _buildSuggestions(),
     );
